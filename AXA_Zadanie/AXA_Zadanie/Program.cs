e6519e99193713aeef2d32b38bcced1398cd7743
alekparol92@gmail.com <40210140+alekparol@users.noreply.github.com>
2020-10-19 23:03:00 +0200

    Adding SearchBox Class

--- AXA_Zadanie/AXA_Zadanie/Program.cs
+++ AXA_Zadanie/AXA_Zadanie/Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace AXA_Zadanie
 {
@@ -10,6 +15,18 @@ namespace AXA_Zadanie
     {
         static void Main(string[] args)
         {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.pl/maps/preview");
+
+            Thread.Sleep(20000);
+
+            SearchBox ne = new SearchBox(driver);
+            ne.ClickDriectionButton();
+
+            Thread.Sleep(2000);
+            driver.Quit();
+
         }
-    }
+	}
 }
---
+++ AXA_Zadanie/AXA_Zadanie/SearchBox.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AXA_Zadanie
+{
+    public class SearchBox
+    {
+
+        /* Fields */
+
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "searchbox")]
+        public IWebElement SearchField { get; set; }
+
+        [FindsBy(How = How.Id, Using = "searchbox-directions")]
+        public IWebElement DirectionsButton { get; set; }
+
+        /* Methods */
+
+        public void ClickDriectionButton()
+        {
+            DirectionsButton.Click();
+        }
+        /* Constructors */
+
+
+        public SearchBox(IWebDriver driver)
+        {
+            this.driver = driver;
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@role=\"region\"]")));
+            PageFactory.InitElements(this, new RetryingElementLocator(driver, TimeSpan.FromSeconds(20)));
+        }
+
+    }
+}
